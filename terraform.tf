d478209a88fa2bcda9e19226a168e6a6d3c65992
Kevin Coxe <kevcoxe@gmail.com>
2023-05-03 23:42:01 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "kevcoxe-tf-aws-backup-infra"
+    key    = "drone-test-tf.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
