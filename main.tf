a63adb1821116ed0ce6919bf944711f849ac3cbc
Kevin Coxe <kevcoxe@gmail.com>
2023-05-03 23:22:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 
 variable "tf_backup_infra_state_bucket_name" {
-  type = string
+  type        = string
   description = "The name of the bucket for the terraform state file S3 bucket"
 }
 
